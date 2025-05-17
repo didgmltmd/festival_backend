efe833b4afd776b4dacb2afe62a51ca2b65e3315
didgmltmd <gomboy11@naver.com>
2025-05-17 22:15:41 +0900

    feat: 오류 수정

--- src/routes/kitchenRoutes.js
+++ src/routes/kitchenRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getZoneOrders, serveItem } = require("../controllers/kitchenController");
 
 router.get("/:zone", getZoneOrders);
-router.patch("/:timestamp/:index/serve", serveItem);
+router.patch("/:timestamp/:itemIndex/serve", serveItem); // ✅ 수정된 부분
 
 module.exports = router;
